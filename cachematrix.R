8156b69fc92708187bec42f3389c81bd48eebfe5
James Pacheco <james.pacheco@gmail.com>
2015-04-25 23:06:20 -0700

    Update code to perform required operations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Due to the costly nature of the 'solve' function, and finding
+## inverses of Matrices in general, to save some cycles, I have created
+## a matrix which can cache its solve result
 
-## Write a short comment describing this function
+## This is the new "data structure", which is the matrix with a memory cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- null
+  }
+  get <- function() x
+  setSolve <- function(solve) i <<- solve
+  getSolve <- function() i
+  list (set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This will check to see if the solve has been stored, returning that if so, calculating new otherwise.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getSolve()
+        if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setSolve(i)
+        i
 }
